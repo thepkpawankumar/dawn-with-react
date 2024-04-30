81db7b117723de85b875af62bdee962015ad2e0b
Pawan <thepkpawankumar@gmail.com>
2024-04-30 22:12:47 +0530

    Initial commit

---
+++ react-framework/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+     <p>Hello from react</p>
+    </>
+  )
+}
+
+export default App
---
+++ react-framework/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        counter: "src/main.tsx"
+      },
+      output: {
+        dir: '../assets',
+        entryFileNames: 'vite-[name].js',
+        chunkFileNames: 'vite-[name].js',
+        assetFileNames: 'vite-[name].[ext]'
+      }
+    },
+    watch: {
+
+    },
+    emptyOutDir: false, // if it is true then it will delete all the files in the assets directory
+  }
+})
